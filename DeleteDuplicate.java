a37991030415d76b7bcca6ec53dda8e6d253313f
Maitree <maitriraiyani7@gmail.com>
2024-07-13 15:49:47 +0530

    Update DeleteDuplicate.java

--- DeleteDuplicate.java
+++ DeleteDuplicate.java
@@ -7,22 +7,31 @@ public class DeleteDuplicate {
             System.out.println("enter a size of array : ");
             int size=sc.nextInt();
             int[] array=new int[size];
-            int[] ans=new int[size+1];
-            int duplicatenum=0;
             
             for(int i=0;i<size;i++){
                 System.out.println("enter an element : ");
                 array[i]=sc.nextInt();
             }
-    
+            
+            int[] ans=new int[size];
+            int duplicatenum=0;
+            
             for(int i=0;i<size;i++){
-                for(int j=i+1;j<size;j++){
-                    if(array[i]==array[j]){
-                        
+                boolean isDuplicate=false;
+                for(int j=0;j<duplicatenum;j++){
+                    if(array[i]==ans[j]){
+                        isDuplicate = true;
+                        break;
                     }
                 }
-    
+                if(!isDuplicate){
+                    ans[duplicatenum]=array[i];
+                    duplicatenum++;
+                }
             }
 
+            for(int i=0;i<duplicatenum;i++){
+                System.out.println(ans[i]);
+            }
         }
-    }
+}
